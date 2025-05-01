ac8dfd3b084907005e8b324f00cb670c9f80ec21
chadcumm <algeron@gmail.com>
2025-05-01 11:19:42 -0500

    v32

--- src/app/component/orders-table/orders-table.component.ts
+++ src/app/component/orders-table/orders-table.component.ts
@@ -362,15 +362,15 @@ InvokeActivateActionPromise(orders: any, activateDate: string) {
       });
 
       Promise.all(activatePromises).then(() => {
-        PowerOrdersMPagesUtils.SignOrders(m_hMOEW);
-        PowerOrdersMPagesUtils.DestroyMOEW(m_hMOEW);
-        
+        return PowerOrdersMPagesUtils.SignOrders(m_hMOEW).then(() => {
+          PowerOrdersMPagesUtils.DestroyMOEW(m_hMOEW);
+          let vLookback = `${this.lookbackNumber},${this.selectedLookback.value}`;
+          let vLookforward = `${this.lookforwardNumber},${this.selectedLookforward.value}`;
+          this.tableRefresh(vLookback, vLookforward, this.orderType);
+        });
       });
     })
   })
-  let vLookback = `${this.lookbackNumber},${this.selectedLookback.value}`;
-  let vLookforward = `${this.lookforwardNumber},${this.selectedLookforward.value}`;
-  this.tableRefresh(vLookback, vLookforward, this.orderType);
 }
 
 async InvokeActivateAction(orders: any, activateDate: string) {
