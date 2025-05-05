b0acbec7fcb2c67ccdc09c3612ff342eaf9ed932
chadcumm <algeron@gmail.com>
2025-05-05 08:14:17 -0700

    dist update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,10 @@ import { FutureorderService } from './service/futureorder.service';
   templateUrl: './app.component.html',
   styleUrls: ['../theme.scss', '../styles.scss'],
   providers: [FutureorderService],
-  encapsulation: ViewEncapsulation.Emulated,
-  host: {
-    '[class]': '"p-component"'
-  }
+  //encapsulation: ViewEncapsulation.Emulated,
+  //host: {
+  //  '[class]': '"p-component"'
+  //}
 })
 export class AppComponent implements OnInit {
 
